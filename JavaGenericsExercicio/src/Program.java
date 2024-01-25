2a443041642cfe96e097c8e51a0f41507a974770
Michael Duarte <97355909+MichaelDu4rte@users.noreply.github.com>
2024-01-25 19:01:16 -0300

    Add files via upload

---
+++ JavaGenericsExercicio/src/Program.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Object> listaCarro = new ArrayList<Object>();
+		listaCarro.add(new Car("gol", "amarelo"));
+		listaCarro.add(new Car("Bmw", "Verde"));
+
+		
+		
+		System.out.println(listaCarro);
+		
+		
+		
+		
+
+	}
+
+}
